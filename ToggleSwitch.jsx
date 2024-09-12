05e4b77e9212c7b26b1366c2173e12edd7b0cd65
manjukashyapmanju <89076206+manjukashyapmanju@users.noreply.github.com>
2024-09-12 17:29:41 +0530

    Update ToggleSwitch.jsx
    Now we can change body background using icons also

--- ToggleSwitch.jsx
+++ ToggleSwitch.jsx
@@ -1,4 +1,5 @@
-import { useState,useEffect } from "react"
+import { useState,useEffect } from "react";
+import { IoIosMoon,IoIosSunny } from "react-icons/io";
 import "./Toggle.css"
 
 export const ToggleSwitch=()=>{
@@ -17,21 +18,25 @@ export const ToggleSwitch=()=>{
             return !isOn;
         });
     }
-    const checkState= isOn? "dark" :"light";
-    
-    const chnageBg= {backgroundColor:isOn? "black" :"gray"};
+    const checkStateIcon = isOn ? <IoIosSunny /> :  <IoIosMoon  style={{color:"white"}}/>;
+     const checkState=isOn? "dark" :"light";
+    const chnageBg= {backgroundColor:isOn? "black" :"white"};
     return(
         <>
+        <section className="sec-switch">
         <div className="toggle-switch" style={chnageBg} onClick={handleToggleSwitch}>
             <div className={`switch ${checkState}`} >
                 {/* two ways:
                 1. isOn? "on" :"off"
                 2. using varible= checkState
                 */}
-                <span className="state">{checkState}</span>
+                <span className="state">{checkStateIcon}</span>
             </div>
         </div>
-<p>Hi</p>
+        </section>
+        <section>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex ut quaerat rerum dolore voluptas aspernatur, dignissimos opti</p>
+        </section>
         </>
     )
 }
